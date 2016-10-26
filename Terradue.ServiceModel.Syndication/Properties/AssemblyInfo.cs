4fc5a4b9053d36a832c19c1f3cbfec9ffe151a04
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-10-26 17:55:37 +0200

    ready to pack 1.0.1
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.ServiceModel.Syndication/Properties/AssemblyInfo.cs
+++ Terradue.ServiceModel.Syndication/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.CompilerServices;
 @{
     DotNetSyndication Software Package implements the ATOM and RSS object representation, serialization and deserialization
 
-    \xrefitem sw_version "Versions" "Software Package Version" 1.0.0
+    \xrefitem sw_version "Versions" "Software Package Version" 1.0.1
 
     \xrefitem sw_link "Link" "Software Package Link" [DotNetSyndication](https://github.com/Terradue/DotNetSyndication)
 
@@ -32,8 +32,8 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
