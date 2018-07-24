388f724319bb1785a3a2b3a9b44767673942bc36
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2018-07-24 17:25:53 +0200

    fixed tests
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@terradue.com>

--- Terradue.ServiceModel.Syndication.Tests/Test.cs
+++ Terradue.ServiceModel.Syndication.Tests/Test.cs
@@ -11,11 +11,11 @@ namespace Terradue.ServiceModel.Syndication.Tests
         [Test()]
         public void TestCase()
         {
-			var feed = SyndicationFeed.Load(XmlReader.Create("../../in/search.atom"));
+			var feed = SyndicationFeed.Load(XmlReader.Create("../in/search.atom"));
 
 			Atom10FeedFormatter formatter = new Atom10FeedFormatter(feed);
             
-			formatter.WriteTo(XmlWriter.Create(new FileStream("../../out/search.atom", FileMode.Create, FileAccess.Write)));
+			formatter.WriteTo(XmlWriter.Create(new FileStream("../out/search.atom", FileMode.Create, FileAccess.Write)));
 
         }
     }
